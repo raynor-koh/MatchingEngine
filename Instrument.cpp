f1777b5275052fae9eac4ffe8fa110feef2f7e2d
faiunix <syemfai@gmail.com>
2024-05-21 15:42:35 +0800

    Add readcsv function and printMap

--- Instrument.cpp
+++ Instrument.cpp
@@ -1,16 +1,66 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <fstream>
+#include <vector>
+#include <sstream> // std::stringstream
+
 using namespace std;
 
 class Instrument {
   public:
-    static unordered_map<string, Instrument> instrumentMap = {};
+    static unordered_map<string, Instrument> instrumentMap;
     string id;
     string currency;
     int lotSize;
 
     static void readcsv(string filename) {
-      
+      ifstream File(filename);
+      if(!File.is_open()) throw runtime_error("Could not open file");
+      string row;
+      getline(File, row);
+      while (getline(File, row)) {
+        stringstream ss(row);
+        string word;
+        vector<string> commaSepValues;
+        while (getline(ss, word, ',')) {
+          commaSepValues.push_back(word);
+        }
+        string readID = "";
+        string readCurrency = "";
+        int readLotsize = 0;
+        for (int i = 0; i < commaSepValues.size(); i++) {
+          if (i == 0) {
+            readID = commaSepValues[i];
+          } else if (i == 1) {
+            readCurrency = commaSepValues[i];
+          } else {
+            readLotsize = stoi(commaSepValues[i]);
+          }
+        }
+        Instrument newInstr = Instrument(readID, readCurrency, readLotsize);
+        Instrument::instrumentMap.insert(make_pair(readID, newInstr));
+        commaSepValues.clear();
+      };
+      File.close();
     }
+
+    Instrument(string id, string currency, int lotSize) {
+      this->id = id;
+      this->currency = currency;
+      this->lotSize = lotSize;
+    }
+
+    static void printMap() {
+      for (auto p : Instrument::instrumentMap) {
+        cout << p.first << " " << p.second.id << " " << p.second.currency << " " << p.second.lotSize << endl;
+      }
+    }
+
+};
+unordered_map<string, Instrument> Instrument::instrumentMap;
+
+int main() {
+  Instrument::readcsv("./example-set/input_instruments.csv");
+  return 0;
 }
